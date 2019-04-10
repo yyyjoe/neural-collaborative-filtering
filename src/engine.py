daf6b1791151908d4a82a59dfc56d125dbfc40c7
JoeChou <yyyjoe94@gmail.com>
2019-04-10 16:47:14 -0500

    Update: train engine

--- src/engine.py
+++ src/engine.py
@@ -1,10 +1,12 @@
 import torch
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
-
+from torch.optim import lr_scheduler
 from utils import save_checkpoint, use_optimizer
 from metrics import MetronAtK
-
+from datetime import datetime
+import os
+from os import path
 
 class Engine(object):
     """Meta Engine for training & evaluating NCF model
@@ -21,14 +23,22 @@ class Engine(object):
         # explicit feedback
         # self.crit = torch.nn.MSELoss()
         # implicit feedback
-        self.crit = torch.nn.BCELoss()
+        self.now = datetime.now()
+        self.date_time = self.now.strftime("%m%d_%H%M%S")
 
-    def train_single_batch(self, users, items, ratings):
+        self.save_dir='checkpoints/' + config['model'] +'/' + self.date_time
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
+
+        self.crit = torch.nn.BCELoss()
+        self.scheduler = lr_scheduler.MultiStepLR(self.opt, milestones=[100,200], gamma=0.1)
+
+    def train_single_batch(self, users, items, ratings, poster_embeddings):
         assert hasattr(self, 'model'), 'Please specify the exact model !'
         if self.config['use_cuda'] is True:
-            users, items, ratings = users.cuda(), items.cuda(), ratings.cuda()
+            users, items, ratings, poster_embeddings = users.cuda(), items.cuda(), ratings.cuda(), poster_embeddings.cuda()
         self.opt.zero_grad()
-        ratings_pred = self.model(users, items)
+        ratings_pred = self.model(users, items,poster_embeddings)
         loss = self.crit(ratings_pred.view(-1), ratings)
         loss.backward()
         self.opt.step()
@@ -41,10 +51,11 @@ class Engine(object):
         total_loss = 0
         for batch_id, batch in enumerate(train_loader):
             assert isinstance(batch[0], torch.LongTensor)
-            user, item, rating = batch[0], batch[1], batch[2]
+            user, item, rating, poster_embedding = batch[0], batch[1], batch[2], batch[3]
             rating = rating.float()
-            loss = self.train_single_batch(user, item, rating)
-            print('[Training Epoch {}] Batch {}, Loss {}'.format(epoch_id, batch_id, loss))
+            loss = self.train_single_batch(user, item, rating,poster_embedding)
+            if(batch_id%300==0):
+                print('[Training Epoch {}] Batch {}, Loss {}'.format(epoch_id, batch_id, loss))
             total_loss += loss
         self._writer.add_scalar('model/loss', total_loss, epoch_id)
 
@@ -54,13 +65,17 @@ class Engine(object):
         with torch.no_grad():
             test_users, test_items = evaluate_data[0], evaluate_data[1]
             negative_users, negative_items = evaluate_data[2], evaluate_data[3]
+            test_embeddings, negative_embeddings = evaluate_data[4], evaluate_data[5]
             if self.config['use_cuda'] is True:
                 test_users = test_users.cuda()
                 test_items = test_items.cuda()
                 negative_users = negative_users.cuda()
                 negative_items = negative_items.cuda()
-            test_scores = self.model(test_users, test_items)
-            negative_scores = self.model(negative_users, negative_items)
+                test_embeddings = test_embeddings.cuda()
+                negative_embeddings = negative_embeddings.cuda()
+
+            test_scores = self.model(test_users, test_items, test_embeddings)
+            negative_scores = self.model(negative_users, negative_items, negative_embeddings)
             if self.config['use_cuda'] is True:
                 test_users = test_users.cpu()
                 test_items = test_items.cpu()
@@ -68,6 +83,7 @@ class Engine(object):
                 negative_users = negative_users.cpu()
                 negative_items = negative_items.cpu()
                 negative_scores = negative_scores.cpu()
+
             self._metron.subjects = [test_users.data.view(-1).tolist(),
                                  test_items.data.view(-1).tolist(),
                                  test_scores.data.view(-1).tolist(),
@@ -82,5 +98,6 @@ class Engine(object):
 
     def save(self, alias, epoch_id, hit_ratio, ndcg):
         assert hasattr(self, 'model'), 'Please specify the exact model !'
-        model_dir = self.config['model_dir'].format(alias, epoch_id, hit_ratio, ndcg)
+        
+        model_dir = self.save_dir + self.config['model_dir'].format(alias, epoch_id, hit_ratio, ndcg)
         save_checkpoint(self.model, model_dir)
